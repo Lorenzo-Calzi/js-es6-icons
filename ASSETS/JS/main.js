8b236a3ba688de8b951212feebf5c7f0e21ffbdc
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-05-14 20:00:04 +0200

    Aggiunto filtro icone

--- ASSETS/JS/main.js
+++ ASSETS/JS/main.js
@@ -97,29 +97,71 @@ const icons = [
 	}
 ];
 
+
 icons.forEach((icon) => {
 
     if(icon.type === 'animal') {
         console.log('okey' + icon.family, icon.prefix + icon.name);
-        document.getElementById('container').insertAdjacentHTML('beforeend', `
-        <div class = "icons">
-            <i class= "${icon.family} ${icon.prefix}${icon.name}"" style="color:rgba(0, 0, 255, 1)"></i>
-        </div>
-        `) 
+        var color = 'rgba(0, 0, 255, 1)';
+      
     } else if (icon.type === 'vegetable') {
-        document.getElementById('container').insertAdjacentHTML('beforeend', `
-        <div class = "icons">
-            <i class= "${icon.family} ${icon.prefix}${icon.name}"" style="color:rgba(255, 165, 0, 1)"></i>
-        </div>
-        `) 
+        var color = 'rgba(255, 165, 0, 1)';
+     
     } else {
-        document.getElementById('container').insertAdjacentHTML('beforeend', `
-        <div class = "icons">
-            <i class= "${icon.family} ${icon.prefix}${icon.name}"" style="color:rgba(128, 0, 128, 1)"></i>
-        </div>
-        `)
-    }
-    
+        var color = 'rgba(128, 0, 128, 1)';
+    } 
+
+    document.getElementById('container').insertAdjacentHTML('beforeend', `
+    <div class = "icons">
+        <i class= "${icon.family} ${icon.prefix}${icon.name}"" style="color:${color}"></i>
+        <h3>${icon.name}</h3>
+    </div>
+    `)  
 });
+
+
+document.getElementById('categorie').addEventListener('change', function(){
+   const type = this.value;
+    if(type!= 'all'){
+    var filteredIcons = icons.filter((item) => {
+        if(item.type == type) {
+            return item
+        }
+    });
+    } else {
+        var filteredIcons = icons;
+    }
+    document.getElementById('container').innerHTML = '';
+
+    filteredIcons.forEach((icon) => {
+    if(icon.type === 'animal') {
+        console.log('okey' + icon.family, icon.prefix + icon.name);
+        var color = 'rgba(0, 0, 255, 1)';
+      
+    } else if (icon.type === 'vegetable') {
+        var color = 'rgba(255, 165, 0, 1)';
+     
+    } else {
+        var color = 'rgba(128, 0, 128, 1)';
+    } 
+    document.getElementById('container').insertAdjacentHTML('beforeend', `
+    <div class = "icons">
+        <i class= "${icon.family} ${icon.prefix}${icon.name}"" style="color:${color}"></i>
+        <h3>${icon.name}</h3>
+    </div>
+    `)  
+   });
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
